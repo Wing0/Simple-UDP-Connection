b56aef33d465a6249258a913b196622777d33325
Oskari Vinko <vinkoo@student.ethz.ch>
2016-10-03 19:13:35 +0200

    Fixes on dependency of external code

--- udp_connection.py
+++ udp_connection.py
@@ -1,6 +1,23 @@
 import json
 import socket
-from ui_collection import warn
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def warn(txt):
+    print (
+        bcolors.WARNING +
+        txt +
+        bcolors.ENDC)
 
 
 class StatusError(Exception):
